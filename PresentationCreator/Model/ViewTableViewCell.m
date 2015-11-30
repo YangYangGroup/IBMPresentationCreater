261962e044d7631da1b173f8faa9f705de3b96f0
sam <hlzhu@cn.ibm.com>
2015-11-30 20:23:37 +0800

    Sam

--- PresentationCreator/Model/ViewTableViewCell.m
+++ PresentationCreator/Model/ViewTableViewCell.m
@@ -18,24 +18,24 @@
         UIFont *fontName = [UIFont fontWithName:@"Arial" size:16.0f];
         
         _imgView = [[UIImageView alloc]init];
-        _imgView.frame = CGRectMake(15, 5, 100, 140);
+        _imgView.frame = CGRectMake(15, 5, 80, 100);
         [self addSubview:_imgView];
         
         _nameLabel = [[UILabel alloc]init];
-        _nameLabel.frame = CGRectMake(100 + 15 +5, 2, KScreenWidth-100-15-20, 100);
+        _nameLabel.frame = CGRectMake(80 + 15 +5, 2, KScreenWidth-80-15-20, 60);
         _nameLabel.font = fontName;
         _nameLabel.lineBreakMode = UILineBreakModeWordWrap;
         _nameLabel.numberOfLines = 0;
         [self addSubview:_nameLabel];
         
         _statusLabel = [[UILabel alloc]init];
-        _statusLabel.frame = CGRectMake(100 + 15 + 5, 100, KScreenWidth-100-15-20, 20);
+        _statusLabel.frame = CGRectMake(80 + 15 + 5, 60, KScreenWidth-80-15-20, 20);
         
         _statusLabel.font = font;
         [self addSubview:_statusLabel];
         
         _dateLabel = [[UILabel alloc]init];
-        _dateLabel.frame = CGRectMake(100 + 15 + 5, 120, KScreenWidth-100-15-20, 20);
+        _dateLabel.frame = CGRectMake(80 + 15 + 5, 80, KScreenWidth-80-15-20, 20);
         _dateLabel.font = font;
         [self addSubview:_dateLabel];
         
--- PresentationCreator/ViewController.m
+++ PresentationCreator/ViewController.m
@@ -73,7 +73,7 @@
 //列表每行的高度
 -  (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 150;
+    return 110;
 }
 //列表每行显示的内容
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
