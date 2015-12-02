0e7b85716cba55074032b4c5b7a8390fab268579
sam <hlzhu@cn.ibm.com>
2015-12-02 08:34:46 +0800

    Sam change page

--- PresentationCreator/Controller/ProjectModel/CreateController/SelectTemplateViewController.m
+++ PresentationCreator/Controller/ProjectModel/CreateController/SelectTemplateViewController.m
@@ -9,11 +9,21 @@
 #import "SelectTemplateViewController.h"
 #import "SelectTemplateCollectionViewCell.h"
 #import "CreationEditViewController.h"
+#import "ShowViewController.h"
 
 @interface SelectTemplateViewController ()<UICollectionViewDataSource,UICollectionViewDelegate>
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) NSArray *imageArray;
 @property (nonatomic, strong) NSArray *titleArray;
+
+@property (nonatomic, strong) NSMutableArray *detailsArray;//获取details表对象
+@property (nonatomic, strong) NSString *maxSummaryIdStr;//取summary表中最大的主键值
+@property (nonatomic, strong) NSString *summaryNameStr;
+@property (nonatomic, strong) UIControl *titleViewControl;
+@property (nonatomic, strong) UITextView *titleTextView;
+@property (nonatomic, strong) UIButton *okButton;
+@property (nonatomic) NSInteger selectTemplateIndex;
+
 @end
 
 @implementation SelectTemplateViewController
@@ -96,12 +106,139 @@
 -(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
 //    SelectTemplateCollectionViewCell * cell = (SelectTemplateCollectionViewCell *)[collectionView cellForItemAtIndexPath:indexPath];
-    CreationEditViewController *creationVC = [[CreationEditViewController alloc]init];
-    
-    creationVC.selectTemplateIndex = indexPath.item;
-    [self.navigationController pushViewController:creationVC animated:YES];
+//    CreationEditViewController *creationVC = [[CreationEditViewController alloc]init];
+//    
+//    creationVC.selectTemplateIndex = indexPath.item;
+//    [self.navigationController pushViewController:creationVC animated:YES];
+    [self addClick];
     
 }
+
+
+#pragma show ppt name function
+//这个是判断没有输入的时候按钮为不可用状态
+-(void)textViewDidChange:(UITextView *)textView{
+    if (_titleTextView.text.length > 0) {
+        _okButton.enabled = YES;
+    }else{
+        _okButton.enabled = NO;
+    }
+}
+-(void)addClick
+{
+    self.navigationController.navigationBarHidden =YES;//隐藏系统自带导航栏按钮
+    _titleViewControl = [[UIControl alloc]initWithFrame:CGRectMake(0, 20, KScreenWidth, KScreenHeight+5)];
+    _titleViewControl.backgroundColor = [UIColor colorWithRed:247/255.0f green:247/255.0f blue:247/255.0f alpha:1];
+    
+    UILabel *titleLabel = [[UILabel alloc]init];
+    titleLabel.frame = CGRectMake(20, 20, KScreenWidth, 30);
+    titleLabel.textColor = [UIColor blackColor];
+    titleLabel.text = @"Presentation name:";
+    [_titleViewControl addSubview:titleLabel];
+    
+    _titleTextView = [[UITextView alloc]init];
+    _titleTextView.frame = CGRectMake(20, 50, KScreenWidth-40, KScreenHeight*0.25);
+    _titleTextView.delegate = self;
+    _titleTextView.layer.borderColor = [UIColor blackColor].CGColor;
+    _titleTextView.layer.borderWidth = 1;
+    _titleTextView.layer.cornerRadius = 6;
+    _titleTextView.layer.masksToBounds = YES;
+    _titleTextView.backgroundColor = [UIColor whiteColor];
+    [_titleTextView becomeFirstResponder];
+    [_titleViewControl addSubview:_titleTextView];
+    
+    
+    //back按钮
+    UIButton *backBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    backBtn.frame = CGRectMake(60 , 30 + KScreenHeight*0.25 + 40, 60, 40);
+    [backBtn setTitle:@"Back" forState:UIControlStateNormal];
+    [backBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    backBtn.backgroundColor = [UIColor grayColor];
+    backBtn.titleLabel.font = [UIFont systemFontOfSize:18.0];
+    [backBtn.layer setMasksToBounds:YES];
+    [backBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateDisabled];
+    //    [backBtn.layer setBorderWidth:1.0];
+    [backBtn.layer setCornerRadius:7.0];
+    backBtn.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    [backBtn addTarget:self action:@selector(backShowViewClick) forControlEvents:UIControlEventTouchUpInside];
+    [_titleViewControl addSubview:backBtn];
+    
+    // 点击OK按钮，保存到summary表中，并返回最大的主键。
+    _okButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    _okButton.frame = CGRectMake(KScreenWidth-60-60 , 30 + KScreenHeight*0.25 + 40, 60, 40);
+    [_okButton setTitle:@"Save" forState:UIControlStateNormal];
+    [_okButton setTitleColor:[UIColor darkTextColor] forState:UIControlStateNormal];
+    _okButton.backgroundColor = [UIColor grayColor];
+    _okButton.titleLabel.font = [UIFont systemFontOfSize:18.0];
+    [_okButton.layer setMasksToBounds:YES];
+    [_okButton setTitleColor:[UIColor whiteColor] forState:UIControlStateDisabled];
+    //    [_okButton.layer setBorderWidth:1.0];
+    [_okButton.layer setCornerRadius:7.0];
+    _okButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    [_okButton addTarget:self action:@selector(saveSummaryTile) forControlEvents:UIControlEventTouchUpInside];
+    _okButton.enabled = NO;
+    [_titleViewControl addSubview:_okButton];
+    
+    [self.view addSubview:_titleViewControl];
+    [self.view bringSubviewToFront:_titleViewControl];
+}
+-(void)backShowViewClick
+{
+    [_titleViewControl removeFromSuperview];
+    _titleViewControl = nil;
+}
+-(void)saveSummaryTile{
+    
+    NSString *temp = [_titleTextView.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    //看剩下的字符串的长度是否为零
+    if ([temp length]!=0) {
+        self.summaryNameStr = _titleTextView.text;
+        self.navigationItem.title=self.summaryNameStr;
+        self.maxSummaryIdStr = [DBDaoHelper insertSummaryWithName:_titleTextView.text];
+        [_titleViewControl removeFromSuperview];
+        _titleViewControl = nil;
+        self.navigationController.navigationBarHidden =NO;
+        
+        if (self.selectTemplateIndex == 0) {
+            
+            //像details表添加2条数据 首页 尾页
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"1" HtmlCode:template_1 PageNumber:@"1"];
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"2" HtmlCode:template_2 PageNumber:@"2"];
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"3" HtmlCode:template_3 PageNumber:@"3"];
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"4" HtmlCode:template_4 PageNumber:@"4"];
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"5" HtmlCode:template_5 PageNumber:@"5"];
+        }else if (self.selectTemplateIndex == 1){
+            //像details表添加2条数据 首页 尾页
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"1" HtmlCode:template_1 PageNumber:@"1"];
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"6" HtmlCode:template_6 PageNumber:@"2"];
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"7" HtmlCode:template_7 PageNumber:@"3"];
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"8" HtmlCode:template_8 PageNumber:@"4"];
+            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"9" HtmlCode:template_9 PageNumber:@"5"];
+            //            [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"5" HtmlCode:template_5 PageNumber:@"6"];
+            //        [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"5" HtmlCode:template_5 PageNumber:@"7"];
+        }
+        
+        
+        ShowViewController *vc = [[ShowViewController alloc]init];
+        
+        vc.showSummaryIdStr = _maxSummaryIdStr;
+        vc.showSummaryNameStr =  _summaryNameStr;
+        vc.showTemplateIdStr = _maxSummaryIdStr;
+        [self.navigationController pushViewController:vc animated:YES];
+        
+        //    //像details表添加2条数据 首页 尾页
+        //    [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"1" HtmlCode:template_1 PageNumber:@"1"];
+        //    [DBDaoHelper insertHtmlToDetailsSummaryIdWith:self.maxSummaryIdStr TemplateId:@"5" HtmlCode:template_5 PageNumber:@"2"];
+        //查询details表
+        self.detailsArray = [DBDaoHelper selectDetailsDataBySummaryId:self.maxSummaryIdStr];
+        [self.collectionView reloadData];
+    }else{
+        UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"" message:@"Please type your presentation name." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        
+        [alertView show ];
+    }
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
